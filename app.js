a37eda6997fa329e4e0280b4a5c9a0a8045bd4e5
Brown <nbrown9@una.edu>
2025-01-21 16:59:59 -0600

    new additions.

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs'); 
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.sendFile('index.html');
 })
 
-app.listen(3000)
+
+
+app.get('/nodemon', function (req, res) {
+  res.send('im in nodemon')})
+
+app.get ('/helloRender', function (req, res) { 
+  res.send('Hello Express from the Real World <br><a href="/">Back to Home</a>')
+});
+
+app.listen(  
+  port,
+  ()=> console.log( 
+    `server is running on...${port}`
+  )
+)
